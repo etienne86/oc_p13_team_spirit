5aac5e9bcd497573d1f2e3ef6d974f897e515ee5
etienne86 <barbier.et@gmail.com>
2020-06-04 17:46:53 +0200

    setup LANGUAGE_CODE = 'fr'

--- config/settings/base.py
+++ config/settings/base.py
@@ -25,7 +25,7 @@ DEBUG = env.bool("DJANGO_DEBUG", False)
 # In Windows, this must be set to your system time zone.
 TIME_ZONE = "Europe/Paris"
 # https://docs.djangoproject.com/en/dev/ref/settings/#language-code
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "fr"
 # https://docs.djangoproject.com/en/dev/ref/settings/#site-id
 SITE_ID = 1
 # https://docs.djangoproject.com/en/dev/ref/settings/#use-i18n
@@ -41,9 +41,7 @@ LOCALE_PATHS = [str(ROOT_DIR / "locale")]
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#databases
 
-DATABASES = {
-    "default": env.db("DATABASE_URL", default="postgres:///teamspirit")
-}
+DATABASES = {"default": env.db("DATABASE_URL", default="postgres:///teamspirit_db")}
 DATABASES["default"]["ATOMIC_REQUESTS"] = True
 
 # URLS
