9157b913498c435a858e3b0449510c40d40af496
etienne86 <barbier.et@gmail.com>
2020-06-04 18:04:41 +0200

    add favicon.png

--- config/settings/base.py
+++ config/settings/base.py
@@ -41,7 +41,7 @@ LOCALE_PATHS = [str(ROOT_DIR / "locale")]
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#databases
 
-DATABASES = {"default": env.db("DATABASE_URL", default="postgres:///teamspirit_db")}
+DATABASES = {"default": env.db("DATABASE_URL", default="postgres:///teamspirit")}
 DATABASES["default"]["ATOMIC_REQUESTS"] = True
 
 # URLS
