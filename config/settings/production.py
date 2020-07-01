f50f298b7b29b46cd928e86793fe3a152e099c0d
etienne86 <barbier.et@gmail.com>
2020-07-01 07:38:00 +0200

    add heroku to ALLOWED_HOSTS

--- config/settings/production.py
+++ config/settings/production.py
@@ -13,6 +13,7 @@ from .base import env
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["team-spirit.fr"])
+ALLOWED_HOSTS += [".herokuapps.com"]
 
 # DATABASES
 # ------------------------------------------------------------------------------
